1c591c4859f7d86b4072d12c86a6ba14f06e0187
Dieter Pisarewski <dieter.pisarewski@arvatosystems.com>
2012-07-11 12:54:14 -0400

    Require RSpec dynamicaly

--- lib/oanda_exchange.rb
+++ lib/oanda_exchange.rb
@@ -1,7 +1,4 @@
 require "oanda_exchange/config"
 require "oanda_exchange/oanda"
 require "oanda_exchange/oanda_bank"
-
-if defined? RSpec
-  require "oanda_exchange/stubs"
-end
+require "oanda_exchange/stubs"
--- lib/oanda_exchange/stubs.rb
+++ lib/oanda_exchange/stubs.rb
@@ -1,11 +1,11 @@
-require 'rspec/mocks/standalone'
-
 module OandaExchange::Stubs
+  require 'rspec/mocks/standalone' if defined? RSpec
 
   DEFAULT_CURRENCIES  = {"USD" => "US Dollar", "EUR" => "Euro"}
   DEFAULT_RATES       = {"USD" => 1, "other" => 1.5}
 
   def self.stub!(options = {})
+    raise "RSpec stub library required" unless Object.respond_to? :stub!
     rates = options[:rates] || DEFAULT_RATES
     Oanda.stub!(:exchange) do |cur, opts|
       BigDecimal.new((opts[:amount] || 0).to_s, 5) * (rates[cur] || rates["other"] || 1)
